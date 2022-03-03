17132d97c06bec22cb943ea44727bd10696198a1
3ddelano <3ddelano@gmail.com>
2022-03-03 21:37:17 +0530

    add gdscript-docs-maker back

---
+++ docs/gdscript-docs-maker/src/gdscript_docs_maker/convert_to_markdown.py
@@ -0,0 +1,290 @@
+"""Parses the JSON data from Godot as a dictionary and outputs markdown
+documents
+
+"""
+import re
+from argparse import Namespace
+from typing import List
+
+from . import mkdocs
+from .command_line import OutputFormats
+from .config import LOGGER
+from .gdscript_objects import Element, GDScriptClass, GDScriptClasses, Function, ProjectInfo, Member, BUILTIN_CLASSES
+from .mkdocs import MkDocsFrontMatter
+from .make_markdown import (
+    MarkdownDocument,
+    MarkdownSection,
+    make_bold,
+    make_code_block,
+    make_comment,
+    make_heading,
+    make_link,
+    make_table_header,
+    make_table_row,
+    surround_with_html,
+    wrap_in_newlines,
+)
+
+GODOT_DOCS_URL = "https://docs.godotengine.org/en/stable/classes/class_{}.html"
+
+def convert_to_markdown(
+    classes: GDScriptClasses, arguments: Namespace, info: ProjectInfo
+) -> List[MarkdownDocument]:
+    """Takes a list of dictionaries that each represent one GDScript class to
+    convert to markdown and returns a list of markdown documents.
+
+    """
+    markdown: List[MarkdownDocument] = []
+    if arguments.make_index:
+        markdown.append(_write_index_page(classes, info))
+    for entry in classes:
+        markdown.append(_as_markdown(classes, entry, arguments))
+    return markdown
+
+
+def _as_markdown(classes: GDScriptClasses, gdscript: GDScriptClass, arguments: Namespace) -> MarkdownDocument:
+    """Converts the data for a GDScript class to a markdown document, using the command line
+    options."""
+
+    content: List[str] = []
+    output_format: OutputFormats = arguments.format
+
+    name: str = gdscript.name
+    if "abstract" in gdscript.metadata.tags:
+        name += " " + surround_with_html("(abstract)", "small")
+
+    if output_format == OutputFormats.MKDOCS:
+        front_matter: MkDocsFrontMatter = MkDocsFrontMatter.from_data(gdscript, arguments)
+        content += front_matter.as_string_list()
+
+    content += [
+        make_comment(
+            "Auto-generated from JSON by GDScript docs maker. "
+            "Do not edit this document directly."
+        )
+        + "\n"
+    ]
+
+    if output_format == OutputFormats.MARDKOWN:
+        content += [*make_heading(name, 1)]
+    
+    if gdscript.extends:
+        extends_list: List[str] = gdscript.get_extends_tree(classes)
+        extends_links = []
+        for entry in extends_list:
+            link = make_link(entry, "../" + entry)
+            if entry.lower() in BUILTIN_CLASSES:
+                link = make_link(entry, GODOT_DOCS_URL.format(entry.lower()))
+            extends_links.append(link) 
+        content += [make_bold("Extends:") + " " + " < ".join(extends_links)]
+    
+
+    description = _replace_references(classes, gdscript, gdscript.description)
+    content += [*MarkdownSection("Description", 2, [description]).as_text()]
+
+    # Insert quick links
+    # content += [
+    #     "",
+    #     "- " + surround_with_html(make_link("Signals", "#signals"), "small"),
+    #     "",
+    #     "- " + surround_with_html(make_link("Enums", "#enumerations"), "small"),
+    #     "",
+    #     "- " + surround_with_html(make_link("Constants", "#constants-descriptions"), "small"),
+    #     "",
+    #     "- " + surround_with_html(make_link("Properties", "#property-descriptions"), "small"),
+    #     "",
+    #     "- " + surround_with_html(make_link("Method Descriptions", "#method-descriptions"), "small"),
+    # ]
+
+    if gdscript.signals:
+        content += MarkdownSection(
+            "Signals", 2, _write_signals(classes, gdscript, output_format)
+        ).as_text()
+    
+    content += _write_class(classes, gdscript, output_format, 2)
+
+    if gdscript.sub_classes:
+        content += make_heading("Sub-classes", 2)
+    for cls in gdscript.sub_classes:
+        content += _write_class(classes, cls, output_format, 3, True)
+
+    return MarkdownDocument(gdscript.name, content)
+
+
+def _write_class(
+    classes: GDScriptClasses,
+    gdscript: GDScriptClass,
+    output_format: OutputFormats,
+    heading_level: int,
+    is_inner_class: bool = False,
+) -> List[str]:
+    markdown: List[str] = []
+    if is_inner_class:
+        markdown += make_heading(gdscript.name, heading_level)
+    for attribute, title in [
+        ("enums", "Enumerations"),
+        ("constants", "Constants Descriptions"),
+        ("members", "Property Descriptions"),
+        ("functions", "Method Descriptions"),
+    ]:
+        if not getattr(gdscript, attribute):
+            continue
+        markdown += MarkdownSection(
+            title,
+            heading_level + 1 if is_inner_class else heading_level,
+            _write(attribute, classes, gdscript, output_format),
+        ).as_text()
+    return markdown
+
+
+def _write_summary(gdscript: GDScriptClass, key: str) -> List[str]:
+    element_list = getattr(gdscript, key)
+    if not element_list:
+        return []
+    markdown: List[str] = make_table_header(["Type", "Name"])
+    return markdown + [make_table_row(item.summarize()) for item in element_list]
+
+
+def _write(
+    attribute: str,
+    classes: GDScriptClasses,
+    gdscript: GDScriptClass,
+    output_format: OutputFormats,
+    heading_level: int = 3,
+) -> List[str]:
+    assert hasattr(gdscript, attribute)
+
+    markdown: List[str] = []
+    for element in getattr(gdscript, attribute):
+        # assert element is Element
+
+        heading = element.get_heading_as_string()
+        if isinstance(element, Member):
+            if element.is_exported:
+                heading += " " + surround_with_html("(export)", "small")
+        markdown.extend(make_heading(heading, heading_level))
+
+        markdown.extend([make_code_block(element.signature), ""])
+        
+        description_first = False
+        if isinstance(element, Function):
+            description_first = True
+
+        unique_attributes = element.get_unique_attributes_as_markdown()
+        unique_attributes = [_replace_references(classes, gdscript, x) for x in unique_attributes]
+        description: str = _replace_references(classes, gdscript, element.description)
+
+        if description_first:
+            markdown.append(description)
+            markdown.append("")
+            markdown.extend(unique_attributes)
+        else:
+            markdown.extend(unique_attributes)
+            markdown.append("")
+            markdown.append(description)
+
+    return markdown
+
+
+def _write_signals(
+    classes: GDScriptClasses, gdscript: GDScriptClass, output_format: OutputFormats
+) -> List[str]:
+
+    ret_signals = []
+    for s in gdscript.signals:
+        signal = "{}\n{}\n{}".format(
+            "".join(make_heading(s.name, 3)),
+            make_code_block(s.signature),
+            _replace_references(classes, gdscript, s.description)
+        )
+        ret_signals.append(signal)
+    return ret_signals
+
+
+def _write_index_page(classes: GDScriptClasses, info: ProjectInfo) -> MarkdownDocument:
+    title: str = "{} ({})".format(info.name, surround_with_html(info.version, "small"))
+    content: List[str] = [
+        *MarkdownSection(title, 1, info.description).as_text(),
+        *MarkdownSection("Contents", 2, _write_table_of_contents(classes)).as_text(),
+    ]
+    return MarkdownDocument("index", content)
+
+
+def _write_table_of_contents(classes: GDScriptClasses) -> List[str]:
+    toc: List[str] = []
+
+    by_category = classes.get_grouped_by_category()
+
+    for group in by_category:
+        indent: str = ""
+        first_class: GDScriptClass = group[0]
+        category: str = first_class.category
+        if category:
+            toc.append("- {}".format(make_bold(category)))
+            indent = "  "
+
+        for gdscript_class in group:
+            link: str = indent + "- " + make_link(
+                gdscript_class.name, gdscript_class.name
+            )
+            toc.append(link)
+
+    return toc
+
+
+def _replace_references(classes: GDScriptClasses, gdscript: GDScriptClass, description: str) -> str:
+    """Finds and replaces references to other classes or methods in the
+    `description`."""
+    ERROR_MESSAGES = {
+        "class": "Class {} not found in the class index.",
+        "member": "Symbol {} not found in {}. The name might be incorrect.",
+    }
+    ERROR_TAIL = "The name might be incorrect."
+    
+    references: re.Match = re.findall(r"\[.+\]", description)
+    for reference in references:
+        # Matches [ClassName], [symbol], and [ClassName.symbol]
+        match: re.Match = re.match(
+            r"\[([A-Z][a-zA-Z0-9]*)?\.?([a-z0-9_]+)?\]", reference
+        )
+        if not match:
+            continue
+
+        class_name, member = match[1], match[2]
+        is_builtin_class = False
+        if class_name and class_name not in classes.class_index:
+            if class_name.lower() in BUILTIN_CLASSES:
+                is_builtin_class = True
+            else:
+                LOGGER.warning(ERROR_MESSAGES["class"].format(class_name) + ERROR_TAIL)
+                continue
+
+        if member and class_name:
+            if member not in classes.class_index[class_name]:
+                LOGGER.warning(
+                    ERROR_MESSAGES["member"].format(member, class_name) + ERROR_TAIL
+                )
+                continue
+        elif member and member not in classes.class_index[gdscript.name]:
+            LOGGER.warning(
+                ERROR_MESSAGES["member"].format(member, gdscript.name) + ERROR_TAIL
+            )
+            continue
+
+        display_text, path = "", "../"
+        if class_name:
+            display_text, path = class_name, class_name
+        if class_name and member:
+            display_text += "."
+            path += "/"
+        if member:
+            display_text += member
+            path += "#" + member.replace("_", "-")
+        
+        if is_builtin_class:
+            display_text = class_name
+            path = GODOT_DOCS_URL.format(class_name.lower())
+
+        link: str = make_link(display_text, path)
+        description = description.replace(reference, link, 1)
+    return description
---
+++ docs/gdscript-docs-maker/src/gdscript_docs_maker/mkdocs.py
@@ -0,0 +1,86 @@
+"""Functions to format the markdown output for the static website engine mkdocs.
+"""
+import datetime
+from argparse import Namespace
+from dataclasses import dataclass
+from typing import List
+from .make_markdown import surround_with_html
+from .config import MKDOCS_FRONT_MATTER, LOGGER
+from .gdscript_objects import GDScriptClass
+
+
+@dataclass
+class MkDocsFrontMatter:
+    """Container for required front matter data for export to mkdocs-friendly
+markdown"""
+
+    title: str
+    description: str
+    tags: List[str]
+
+    def as_string_list(self) -> List[str]:
+        hide_sections = []
+        if "hide_toc" in self.tags:
+            hide_sections.append("toc")
+            self.tags.remove("hide_toc")
+        if "hide_navigation" in self.tags:
+            hide_sections.append("navigation")
+            self.tags.remove("hide_navigation")
+
+        hide_str = "\n".join(["  - " + section for section in hide_sections])
+        if hide_str != "":
+            hide_str = "hide:\n" + hide_str 
+
+        tags_str = "\n".join(["  - " + tag for tag in self.tags])
+        if tags_str != "":
+            tags_str = "tags:\n" + tags_str
+        
+        strings: List[str] = [
+            self.title,
+            self.description,
+            tags_str,
+            hide_str
+        ]
+        LOGGER.debug("MKDOCS front matter:\n" + repr(strings))
+        # strings = list(map(quote_string, strings))
+        ret = [MKDOCS_FRONT_MATTER["toml"].format(*strings)]
+        return ret
+
+    @classmethod
+    def from_data(cls, gdscript: GDScriptClass, arguments: Namespace):
+        name: str = gdscript.name
+        if "abstract" in gdscript.metadata.tags:
+            name += " " + surround_with_html("(abstract)", "small")
+        if "class" in gdscript.metadata.tags:
+            name: str = "Class " + name
+
+        
+        return MkDocsFrontMatter(
+            name,
+            gdscript.description.replace("\n", "\\n"),
+            gdscript.metadata.tags,
+        )
+
+
+FRONT_MATTER_DEFAULT: MkDocsFrontMatter = MkDocsFrontMatter(
+    "", "", []
+)
+
+
+def make_relref(target_document: str, language: str = "gdscript") -> str:
+    """Returns a {{< relref >}} shortcode as a string."""
+    return make_shortcode(target_document, "relref")
+
+
+def make_shortcode(content: str, shortcode: str, *arguments: str, **kwargs: str) -> str:
+    """Returns a shortcode built from the arguments, with the form
+    {{< shortcode *args **kwargs >}}content{{< / shortcode >}}"""
+    key_value_pairs: str = " ".join(["{}={}" for key, value in kwargs.items()])
+    return "{{{{< {0} {1} {2} >}}}}{3}{{{{< / {0} >}}}}".format(
+        shortcode, " ".join(arguments), key_value_pairs, content
+    )
+
+
+def quote_string(text: str) -> str:
+    """Quotes and returns the text with escaped \" characters."""
+    return '"' + text.replace('"', '\\"') + '"'
